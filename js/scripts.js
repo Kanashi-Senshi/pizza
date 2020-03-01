a9f93e1d37061d1d6d9865ad5847617a9b5ad281
Kanashi-Senshi <mwashamichael@gmail.com>
2020-03-01 22:46:59 +0300

    Adds an order section after click on order online

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var modal = document.getElementById("my-modal-order-page");
+var btn = document.getElementById("order-button-load-page");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
